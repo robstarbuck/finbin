ac1380e706bfcfe08c51913c104fc1f81b82377a
robstarbuck <robstarbuck@gmail.com>
2022-02-25 10:38:32 +0000

    Removed react version

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,9 +32,4 @@ module.exports = {
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off",
   },
-  settings: {
-    react: {
-      version: "detect",
-    },
-  },
 };
